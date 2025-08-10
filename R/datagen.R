48ec9568ca50b4714a7c5493a50b0aed0dfd2b40
PengSU517 <pengsu666@gmail.com>
2025-08-10 21:31:35 +0800

    优化

--- R/datagen.R
+++ R/datagen.R
@@ -14,20 +14,21 @@ datagen = function(n, p, tau, distri, heter) {
   # distri = "normal" # "cauchy"
   # heter = 1
 
-  beta = c(1, 0.8, 0.6, 0.4, 0.2, rep(0, p - 5))
-  lambda = c(1,1,1)
+  beta = c(1, 0, -1, 0, 1)
+  lambda = c(1,0,1,0,1)
   mean = rep(0, p)
-  sigmax = diag(rep(1, p))
-  r = 0.5
-  for (i in 1:p) {
-    for (j in 1:p) {
-      if (i != j)
-        sigmax[i, j] = sqrt(sigmax[i, i] * sigmax[j, j]) * r^abs(i - j)
-    }
-  }
+  # sigmax = diag(rep(1, p))
+  # r = 0.5
+  # for (i in 1:p) {
+  #   for (j in 1:p) {
+  #     if (i != j)
+  #       sigmax[i, j] = sqrt(sigmax[i, i] * sigmax[j, j]) * r^abs(i - j)
+  #   }
+  # }
 
   ## generate datasets
-  x = MASS::mvrnorm(n, mean, sigmax)
+  x = matrix(rnorm(n * p), nrow = n, ncol = p)
+  # x = MASS::mvrnorm(n, mean, sigmax)
   if (distri == "cauchy") {
     disfunc = rcauchy
     quantfunc = qcauchy
@@ -36,8 +37,8 @@ datagen = function(n, p, tau, distri, heter) {
     quantfunc = qnorm
   }
 
-  eps = (disfunc(n, 0, 1) - quantfunc(tau)) *  exp(heter * (x[,1:3] %*% lambda)) +  quantfunc(tau)
-  t = x %*% beta + eps
+  eps = (disfunc(n, 0, 1) - quantfunc(tau)) *  exp(heter * (x[,1:5] %*% lambda)) +  quantfunc(tau)
+  t = x[,1:5] %*% beta + eps
   c = runif(n, 0, 3) ## generate censoring time
   y = pmin(t, c)
   d = as.numeric(t < c)
