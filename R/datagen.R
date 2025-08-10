f049eb808b9bb96336faeb307f3af6eb623d51c8
PengSU517 <pengsu666@gmail.com>
2025-08-10 17:33:43 +0800

    优化代码

---
+++ R/datagen.R
@@ -0,0 +1,56 @@
+#' Data generation function for survival analysis
+#' This function generates survival data based on specified parameters.
+#' @param n Number of samples to generate.
+#' @param p Number of features.
+#' @param tau Quantile level for the error term.
+#' @param distri Distribution of the error term, either "normal" or "cauchy".
+#' @param heter Heterogeneity parameter for the error term.
+
+datagen = function(n, p, tau, distri, heter) {
+
+  # n = 100
+  # p = 10
+  # tau = 0.5
+  # distri = "normal" # "cauchy"
+  # heter = 1
+
+  beta = c(1, 0.8, 0.6, 0.4, 0.2, rep(0, p - 5))
+  lambda = c(1,1,1)
+  mean = rep(0, p)
+  sigmax = diag(rep(1, p))
+  r = 0.5
+  for (i in 1:p) {
+    for (j in 1:p) {
+      if (i != j)
+        sigmax[i, j] = sqrt(sigmax[i, i] * sigmax[j, j]) * r^abs(i - j)
+    }
+  }
+
+  ## generate datasets
+  x = MASS::mvrnorm(n, mean, sigmax)
+  if (distri == "cauchy") {
+    disfunc = rcauchy
+    quantfunc = qcauchy
+  } else {
+    disfunc = rnorm
+    quantfunc = qnorm
+  }
+
+  eps = (disfunc(n, 0, 1) - quantfunc(tau)) *  exp(heter * (x[,1:3] %*% lambda)) +  quantfunc(tau)
+  t = x %*% beta + eps
+  c = runif(n, 0, 3) ## generate censoring time
+  y = pmin(t, c)
+  d = as.numeric(t < c)
+  # sum(d)
+
+  return(list(
+    y = y,
+    d =  d,
+    x = x,
+    t = t,
+    c = c,
+    rate = 1 - sum(d) / n,
+    beta = beta,
+    lambda = lambda
+  ))
+}
